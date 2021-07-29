680d6894719e0e5cf7becb187f4efb00e485340c
SanppyDev <87994270+SanppyDev@users.noreply.github.com>
2021-07-29 14:11:43 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,31 +1,12 @@
 var login = document.querySelector('.entrar');
+let closeBtn = document.querySelector('.close')
+let mobMenu = document.querySelector('.mob__menu')
+let hamburguerMenu = document.querySelector(".hamburguer__menu");
 
-function logador(){
-    login.addEventListener('click', ()=>{
-        let loginBtn = document.querySelector('.login__btn');
-        let login = document.querySelector('.login');
-        let loginClose= document.querySelector('.close')
-        
-        login.style.display = "flex";
-        loginClose.addEventListener('click', ()=> {
-            login.style.display = "none";
-        })
-            
-        
-        loginBtn.addEventListener('click', ()=>{
-            let nome = document.getElementById('nome').value;
-            let senha = document.getElementById('senha').value;
-            if((nome == "UsuarioTeste") && (senha == "123")){
-                window.location.href="html/documentos.html"
-            }else{
-                window.alert("Usuario ou senha incorretos!")
-                document.getElementById('nome').value= "";
-                document.getElementById('senha').value= "";
-            }
-        })
-    })
-}
+hamburguerMenu.addEventListener('click', ()=> {
+    mobMenu.style.right = "0";
+})
 
-
-logador()
-
+closeBtn.addEventListener('click', () => {
+    mobMenu.style.right = "-500px";
+})
